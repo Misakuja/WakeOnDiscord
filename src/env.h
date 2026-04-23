1647866df7996f17608dfe8d1a986da3230052f6
Misakuja <156021641+Misakuja@users.noreply.github.com>
2026-04-23 17:52:07 +0200

    env.h base added

---
+++ src/env.h
@@ -0,0 +1,14 @@
+#ifndef WAKEONDISCORD_ENV_H
+#define WAKEONDISCORD_ENV_H
+
+const char* WIFI_SSID = "SSID";
+const char* WIFI_PASSWORD = "PASSWORD";
+
+const char* BOT_TOKEN = "BOT_TOKEN";
+const char* USER_ID = "YOUR_USER_ID";
+
+const char* TARGET_PC_MAC = "YOUR_PC_MAC";
+
+const char* CHANNEL_ID = "YOUR_CHANNEL_ID";
+
+#endif
